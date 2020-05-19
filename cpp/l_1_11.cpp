c91cb7810553e35c6e01c796f45468a7db668c10
foo7print <foo7print@users.noreply.github.com>
2020-05-19 20:16:10 +0900

    EX11.電卓をつくろう2
    L - 1.11.for文・break・continue

---
+++ cpp/l_1_11.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int N, A;
+  cin >> N >> A;
+
+  // ここにプログラムを追記
+  for (int i = 0; i < N; i++) {
+    string op;
+    int x;
+    cin >> op >> x;
+
+    if (op == "+") {
+      A += x;
+    }
+    else if (op == "-") {
+      A -= x;
+    }
+    else if (op == "*") {
+      A *= x;
+    }
+    else if (op == "/" && x != 0) {
+      A /= x;
+    }
+    else {
+      cout << "error" << endl;
+      break;
+    }
+    cout << i + 1 << ":" << A << endl;
+  }
+}
