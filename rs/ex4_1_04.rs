197ba1ce8b6f3545d0062425f3bd45226cc335dd
foo7print <foo7print@users.noreply.github.com>
2021-03-14 16:58:41 +0900

    ex4_1_04.rs

---
+++ rs/ex4_1_04.rs
@@ -0,0 +1,13 @@
+fn main() {
+  // 一年の秒数
+  let seconds: i32 = 365 * 24 * 60 * 60;
+  /* 1年は何秒か */
+  println!("{}", seconds);
+  /* 2年は何秒か */
+  println!("{}", seconds * 2);
+  /* 5年は何秒か */
+  println!("{}", seconds * 5);
+  /* 10年は何秒か */
+  println!("{}", seconds * 10);
+}
+
